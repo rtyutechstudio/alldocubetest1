bbd43ccf0f28edf8558b202d07b55727fb395ef0
rk3288 <m18749410674@163.com>
2026-05-16 18:15:09 +0800

    Update twrp_iPlay50_Pro.mk

--- twrp_iPlay50_Pro.mk
+++ twrp_iPlay50_Pro.mk
@@ -11,8 +11,8 @@ $(call inherit-product, device/alldocube/t1030m/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := twrp_iPlay50_Pro
-PRODUCT_DEVICE := iPlay50_Pro
+PRODUCT_NAME := t1030m
+PRODUCT_DEVICE := t1030m
 PRODUCT_BRAND := Alldocube
 PRODUCT_MODEL := iPlay 50 Pro
 PRODUCT_MANUFACTURER := Alldocube
@@ -20,6 +20,6 @@ PRODUCT_MANUFACTURER := Alldocube
 PRODUCT_GMS_CLIENTID_BASE := android-alldocube
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=iPlay50_Pro \
-    PRODUCT_DEVICE=T1030M \
+    PRODUCT_NAME=t1030m \
+    PRODUCT_DEVICE=t1030m \
     PRIVATE_BUILD_DESC="Alldocube/iPlay50_Pro/T1030M:13/TP1A.220624.014/1689926629:user/release-keys"
